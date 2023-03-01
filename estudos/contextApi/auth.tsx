4e3f369e22d312cfdc3df3de716206839b7ab253
Ronaldo Silva <ronaldo.intermec@gmail.com>
2023-03-01 13:16:49 -0300

    final commit

--- estudos/contextApi/auth.tsx
+++ estudos/contextApi/auth.tsx
@@ -1,27 +1,24 @@
-import React, { createContext, useState } from "react";
+import React, {createContext, useState} from 'react';
 
 export const AuthContext = createContext({});
 
-function AuthProvider({ children }: { children: any }) {
+function AuthProvider({children}: {children: any}) {
+  const [user, setUser] = useState({});
 
-    const [user, setUser] = useState({});
-
-    function signIn(email: any, password: any, navigation: any) {
-        if (email !== '' && password !== '') {
-            setUser({
-                email: email,
-                status: 'Ativo'
-            });
-         navigation.navigate("Home");
-        }
-
+  function signIn(email: any, password: any, navigation: any) {
+    if (email !== '' && password !== '') {
+      setUser({
+        email: email,
+        status: 'Ativo',
+      });
+      navigation.navigate('Home');
     }
+  }
 
-    return (
-        <AuthContext.Provider value={{ name: 'Bruna Oliveira', signIn,user }} >
-            {children}
-        </AuthContext.Provider>)
+  return (
+    <AuthContext.Provider value={{name: 'Bruna Oliveira', signIn, user}}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 export default AuthProvider;
-
-
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { AppRegistry } from 'react-native';
+import {AppRegistry} from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
+import {name as appName} from './app.json';
 import Counter from './estudos/counter';
 import FieldInput from './estudos/input/textInput';
 import List from './estudos/list/list';
@@ -12,26 +12,22 @@ import CustomComponentsProps from './estudos/custom/customComponentsProps';
 import Navigation from './estudos/navigation/navigation';
 import TabNavigator from './estudos/tabNavigation/tabNavigator';
 import DrawerSideMenu from './estudos/drawerSideMenu/drawerSideMenu';
-import PassingDataBetweenScreens from './estudos/passingDateBetweenScreens/passingDataBetweenScreens'
+import PassingDataBetweenScreens from './estudos/passingDateBetweenScreens/passingDataBetweenScreens';
 import GlobalStyleCustom from './estudos/globalStyle/GlobalStyleCustom';
 import AsyncStorage from './estudos/asyncStorage/asyncStorage';
 import SqliteDataBase from './estudos/sqlite/sqliteDataBase';
 import ReduxStateManagement from './estudos/reduxStateManagement/reduxStateManagement';
 import FetchApiData from './estudos/fetchAPIData/fetchAPIData';
-import LocalScheduledPushNotification from './estudos/localScheduledPushNotification/localScheduledPushNotification'
+import LocalScheduledPushNotification from './estudos/localScheduledPushNotification/localScheduledPushNotification';
 //import Index from './estudos/contextApi/index'
 //import Routes from './estudos/contextApi/routes'
 
-
-import PushNotification from "react-native-push-notification";
+import PushNotification from 'react-native-push-notification';
 PushNotification.configure({
-    onNotification: function (notification) {
-        console.log("NOTIFICATION:", notification);
-
-      },
-      requestPermissions: Platform.OS === 'ios'
-}
-);
+  onNotification: function (notification) {
+    console.log('NOTIFICATION:', notification);
+  },
+  requestPermissions: Platform.OS === 'ios',
+});
 
 AppRegistry.registerComponent(appName, () => LocalScheduledPushNotification);
-
